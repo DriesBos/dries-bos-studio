cd04a3388a9cfb0633f111ffcc61411bc77db68c
Dries Bos <info@driesbos.com>
2019-03-15 15:50:39 +0100

    Heroku deployment scripts/P rocfile

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Boilerplate-Nuxt-Storyblok',
+    title: 'nuxt-storyblok-boilerplate',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
