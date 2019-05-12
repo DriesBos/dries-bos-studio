07a1b1ce9dd85e107cc0b0f508044aaf8a2a05b3
Dries Bos <info@driesbos.com>
2019-05-12 21:26:39 +0200

    header update

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,10 +1,17 @@
 <template>
   <header>
-    <nuxt-link class="header-Logo" to="/" tag="div">
-      <a>Dries Bos Studio</a>
-    </nuxt-link>
-    <nuxt-link to="/about" tag="div">
+    <div class="header-Logo">
+      <nuxt-link class="header-Link" to="/" tag="a">
+        <span v-if="$route.path !== '/about'">Dries</span>
+        <span v-if="$route.path === '/'">Bos Studio</span>
+        <span v-if="$route.name === 'blog-postId'">and {{ blog-postId }}</span>
+      </nuxt-link>
+    </div>
+    <nuxt-link v-if="$route.path === '/'" class to="/about" tag="div">
       <a>Profile</a>
+    </nuxt-link>
+    <nuxt-link v-if="$route.path !== '/'" class to="/" tag="div">
+      <a>X</a>
     </nuxt-link>
   </header>
 </template>
