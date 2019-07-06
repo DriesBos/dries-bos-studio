b094a45da7c7767cbcf68f98dd0284e9ff844583
Dries Bos <info@driesbos.com>
2019-07-06 17:55:48 +0200

    PWA

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,7 @@ module.exports = {
    */
   head: {
     title: 'Dries Bos Studio',
-    meta: [
-      {
+    meta: [{
         charset: 'utf-8'
       },
       {
@@ -21,15 +20,21 @@ module.exports = {
         hid: 'description',
         name: 'description',
         content: 'Meta description'
-      }
-    ],
-    link: [
+      },
       {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
-      }
-    ]
+        name = 'apple-mobile-web-app-capable',
+        content = 'yes'
+      },
+      {
+        name = 'mobile-web-app-capable',
+        content = 'yes'
+      },
+    ],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
 
   /*
@@ -45,7 +50,7 @@ module.exports = {
    ** Router behaviour
    */
   router: {
-    scrollBehavior: function(to, from, savedPosition) {
+    scrollBehavior: function (to, from, savedPosition) {
       return {
         x: 0,
         y: 0
@@ -76,10 +81,11 @@ module.exports = {
     [
       'storyblok-nuxt',
       {
-        accessToken:
-          process.env.NODE_ENV === 'production' // Generate new token
-            ? 'jqAz0BlSNQQGVE8GhfrrFwtt' // Public
-            : 'QMGXgQtF6vZkJUrEKUBIlQtt', // Preview
+        accessToken: process.env.NODE_ENV === 'production' // Generate new token
+          ?
+          'jqAz0BlSNQQGVE8GhfrrFwtt' // Public
+          :
+          'QMGXgQtF6vZkJUrEKUBIlQtt', // Preview
         cacheProvider: 'memory'
       }
     ]
@@ -88,11 +94,11 @@ module.exports = {
    ** Generating routes
    */
   generate: {
-    routes: function() {
+    routes: function () {
       return axios
         .get(
           'https://api.storyblok.com/v1/cdn/stories?version=published&token=QMGXgQtF6vZkJUrEKUBIlQtt&starts_with=blog&cv=' +
-            Math.floor(Date.now() / 1e3)
+          Math.floor(Date.now() / 1e3)
         )
         .then(res => {
           const blogPosts = res.data.stories.map(bp => bp.full_slug)
