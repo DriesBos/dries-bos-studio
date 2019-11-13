fa1d16d155471828279636316b552afc49835f18
Dries Bos <info@driesbos.com>
2019-11-13 15:13:16 +0100

    Google Analytics settings

--- plugins/vue-analytics.js
+++ plugins/vue-analytics.js
@@ -2,7 +2,5 @@ import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
 
 Vue.use(VueAnalytics, {
-  id: 'UA-151943071-1',
-  router,
-  checkDuplicatedScript: true
+  id: 'UA-151943071-1'
 })
