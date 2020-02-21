c9b6b6fdc0b7147ad3ef3b7281e54308b09b4fb4
Dries Bos <info@driesbos.com>
2020-02-21 23:19:27 +0100

    feat: four themes

--- components/IndexListItem.vue
+++ components/IndexListItem.vue
@@ -1,36 +1,22 @@
 <template>
   <li :key="post.id" :id="post.id" class="listItem listItem_Project">
     <div class="listItem-Thumb">
-      <img :src="post.cover_image" alt="" />
+      <img :src="post.cover_image" alt />
     </div>
-    <nuxt-link
-      :to="`/projects/${post.id}`"
-      class="listItem-DetailsWrapper"
-      tag="div"
-    >
+    <nuxt-link :to="`/projects/${post.id}`" class="listItem-DetailsWrapper" tag="div">
       <div class="listItem-Number listItem-Details ellipsis">
         <p>001</p>
       </div>
       <div v-if="post.title" class="listItem-Title listItem-Details ellipsis">
         <p>{{ post.title }}</p>
       </div>
-      <div
-        v-if="post.category"
-        class="listItem-Category listItem-Details ellipsis"
-      >
+      <div v-if="post.category" class="listItem-Category listItem-Details ellipsis">
         <p>{{ post.category }}</p>
       </div>
     </nuxt-link>
     <div class="listItem-Icons">
       <div class="icon-Container" title="view project">
-        <svg width="25" height="25" viewBox="0 0 25 25" class="icon">
-          <path
-            d="M13.5101551,-0.0106601718 L13.5094466,21.1873398 L21.0001551,13.6964466 L22.4143687,15.1106602 L12.5148737,25.0101551 L12.5104466,25.0053398 L12.5059415,25.0101551 L2.60644661,15.1106602 L4.02066017,13.6964466 L11.5094466,21.1853398 L11.5101551,-0.0106601718 L13.5101551,-0.0106601718 Z"
-            fill
-            transform="translate(12.510408, 12.499747) scale(-1, -1) rotate(90.000000) translate(-12.510408, -12.499747)"
-            fill-rule="evenodd"
-          />
-        </svg>
+        <div class="icon arrow" v-html="require('~/assets/images/icon-arrow.svg?include')"></div>
       </div>
     </div>
   </li>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
-  <main :class="{ active: currentTheme }">
-    <div class="background-One"></div>
+  <main :class="currentTheme">
+    <div class="background background-Two"></div>
+    <div class="background background-Three"></div>
+    <div class="background background-Four"></div>
     <the-header @clicked="changeTheme" />
     <transition name="page" mode="out-in">
       <nuxt />
@@ -16,12 +18,27 @@ export default {
   },
   data() {
     return {
-      currentTheme: false
+      themeNumber: 0,
+      currentTheme: "one"
     }
   },
   methods: {
     changeTheme() {
-      this.currentTheme = !this.currentTheme
+      if (this.themeNumber < 3) {
+        this.themeNumber++
+      } else {
+        this.themeNumber = 0
+      }
+      if (this.themeNumber === 1) {
+        this.currentTheme = "two"
+      } else if (this.themeNumber === 2) {
+        this.currentTheme = "three"
+      } else if (this.themeNumber === 3) {
+        this.currentTheme = "four"
+      } else {
+        this.currentTheme = "One"
+      }
+      // console.log(this.themeNumber, this.currentTheme)
     }
   }
 }
