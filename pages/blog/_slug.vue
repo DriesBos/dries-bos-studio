04645371941d04d1b64c452e348b921f7fb264ef
Dries Bos <info@driesbos.com>
2020-02-21 20:50:05 +0100

    style: projectPage imageGrid

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -5,13 +5,9 @@
         <div class="projectItem-Text">
           <markdown-item :input="story.content.content" />
         </div>
-        <ul
-          v-for="(image, index) in story.content.images"
-          :key="index"
-          class="projectItem-Images"
-        >
-          <li>
-            <img :src="image.filename" />
+        <ul class="projectItem-Images">
+          <li v-for="(image, index) in story.content.images" :key="index">
+            <img :src="image.filename" :alt="image.name" />
           </li>
         </ul>
       </div>
