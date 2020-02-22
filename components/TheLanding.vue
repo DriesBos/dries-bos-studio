4597ed6cd16cc631b90ef2da5362486b583df7ff
Dries Bos <info@driesbos.com>
2020-02-22 07:58:26 +0100

    add: landing start

---
+++ components/TheLanding.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="landing-One">
+      <h1>Dries Bos</h1>
+    </div>
+    <div class="landing-Two">
+      <div class="landing-Circle"></div>
+    </div>
+    <div class="landing-Three">
+      <!-- opaque glass -->
+    </div>
+  </div>
+</template>
