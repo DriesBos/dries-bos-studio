deb03f28923096b6b4c393cefaa5e55672a624b7
Dries Bos <info@driesbos.com>
2020-03-02 08:52:04 +0100

    feat: image optimisation

--- plugins/filters.js
+++ plugins/filters.js
@@ -2,9 +2,12 @@ import Vue from "vue"
 import VueLazyload from "vue-lazyload"
 
 /*eslint-disable */
-Vue.filter("resize", (image, option) => {
-  var imageService = "//img2.storyblok.com/"
-  var path = image.replace("//a.storyblok.com", "")
+Vue.filter("transformImage", (image, option) => {
+  if (!image) return ""
+  if (!option) return ""
+
+  let imageService = "//img2.storyblok.com/"
+  let path = image.replace("//a.storyblok.com", "")
   return imageService + option + path
 })
 /* eslint-enable */
