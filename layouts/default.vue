cbeeb1a9139415638b563db9dac493f38d420ca3
Dries Bos <info@driesbos.com>
2020-03-03 23:30:47 +0100

    style: transition to fade

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <main :class="currentTheme">
     <the-header @toggleTheme="changeTheme" />
-    <transition name="page" mode="out-in">
+    <transition name="left" mode="out-in">
       <div class="transitionWrapper">
         <nuxt />
       </div>
