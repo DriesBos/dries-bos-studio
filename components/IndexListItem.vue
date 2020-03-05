7db95c9f2b76702b37c0879cb54ba2b0fe3713a1
Dries Bos <info@driesbos.com>
2020-03-05 12:14:36 +0100

    feat: listItem thumbcircle

--- components/IndexListItem.vue
+++ components/IndexListItem.vue
@@ -10,14 +10,19 @@
       <div v-if="post.category" class="listItem-Category listItem-Details ellipsis">
         <p title="project category">{{ post.category || "" }}</p>
       </div>
+      <div class="listItem-Icons">
+        <div class="icon-Container" title="view project">
+          <div
+            class="icon arrow"
+            v-html="require('~/assets/images/icon-arrow.svg?include')"
+            title="go to project"
+          ></div>
+        </div>
+      </div>
     </nuxt-link>
-    <div class="listItem-Icons">
-      <div class="icon-Container" title="view project">
-        <div
-          class="icon arrow"
-          v-html="require('~/assets/images/icon-arrow.svg?include')"
-          title="go to project"
-        ></div>
+    <div class="listItem-Thumb">
+      <div class="listItem-Thumb_Container">
+        <img :src="post.cover_image" />
       </div>
     </div>
   </li>
@@ -29,6 +34,8 @@ export default {
   props: {
     post: Object
   },
-  mounted() {}
+  mounted() {
+    console.log(this.post)
+  }
 }
 </script>
