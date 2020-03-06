0c097635c8b82b664c802ce5079ce9841a04f54f
Dries Bos <info@driesbos.com>
2020-03-06 11:01:55 +0100

    style: icons update

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -11,7 +11,7 @@
           class="projectItem-Nav projectItem-Nav_Prev"
           title="previous project"
         >
-          <div class="icon arrow" v-html="require('~/assets/images/icon-arrow.svg?include')" />
+          <div class="icon arrow" v-html="require('~/assets/images/icon-arrow-long.svg?include')" />
         </nuxt-link>
         <nuxt-link
           v-if="story.content.next_link.id !== ''"
@@ -19,7 +19,7 @@
           class="projectItem-Nav projectItem-Nav_Next"
           title="next project"
         >
-          <div class="icon arrow" v-html="require('~/assets/images/icon-arrow.svg?include')" />
+          <div class="icon arrow" v-html="require('~/assets/images/icon-arrow-long.svg?include')" />
         </nuxt-link>
         <ul id="projectItem-Images" class="projectItem-Images">
           <li v-for="(image, index) in story.content.images" :key="index">
