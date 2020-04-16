ba132311f52e114151f7f088b8849e08f0838552
Dries Bos <info@driesbos.com>
2020-04-16 23:18:16 +0200

    added hyperlink gsap-animation

--- components/MarkdownItem.vue
+++ components/MarkdownItem.vue
@@ -3,19 +3,19 @@
 </template>
 
 <script>
+import { gsap } from "gsap"
 // https://vuejs.org/v2/examples/index.html
 // install 'Marked' as dependency: npm install marked
-const marked = require('marked')
+const marked = require("marked")
 const renderer = new marked.Renderer()
-// renderer.link = (href, title, text) =>
-//   `<a target="_blank" href="${href}" title="${title}">${text}</a>`
+
 marked.setOptions({
   renderer: new marked.Renderer(),
   breaks: true
 })
 
 export default {
-  name: 'MarkdownItem',
+  name: "MarkdownItem",
   props: {
     input: {
       type: String,
@@ -26,6 +26,14 @@ export default {
     compiledMarkdown: function() {
       return marked(this.input)
     }
+  },
+  mounted() {
+    gsap.to(".markdown", {
+      "--var": "100%",
+      duration: 0.66,
+      stagger: 1,
+      ease: "ease"
+    })
   }
 }
 </script>
@@ -34,6 +42,7 @@ export default {
 @import '~/assets/styling/variables.sass'
 
 .markdown
+  --var: 0%
   min-width: 0 // Ellipsis hack
   p
     margin-bottom: $lineheight-base
@@ -41,5 +50,17 @@ export default {
   p:last-child
     margin-bottom: 0
   a
-    text-decoration: underline
+    position: relative
+    &:after
+      content: ''
+      position: absolute
+      left: 0
+      display: inline-blick
+      bottom: 0
+      width: var(--var)
+      border-bottom: 1px solid var(--support-color)
+    &:hover
+      &:after
+        animation: hyperHyperLink .66s ease
+        animation-iteration-count: 1
 </style>
