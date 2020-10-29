62df720f1a0bfbe9adf28482e78f59a111037feb
Dries Bos <driesbos@MBP-van-Dries.home>
2020-10-29 17:09:30 +0100

    added project page

---
+++ components/Project.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-editable="blok" class="view view-Project">
+    <component
+      :is="blok.component | dashify"
+      v-for="blok in blok.body"
+      :key="blok._uid"
+      :blok="blok"
+    ></component>
+  </div>
+</template>
+
+<script>
+export default {
+  scrollToTop: true,
+  props: {
+    blok: Object
+  }
+}
+</script>
--- plugins/components.js
+++ plugins/components.js
@@ -1,8 +1,10 @@
 import Vue from "vue"
 import Page from "~/components/Page.vue"
+import Project from "~/components/Project.vue"
 import ItemText from "~/components/ItemText.vue"
 import ItemColumn from "~/components/ItemColumn.vue"
 
 Vue.component("blok-page", Page)
+Vue.component("blok-project", Project)
 Vue.component("blok-item-text", ItemText)
 Vue.component("blok-item-column", ItemColumn)
