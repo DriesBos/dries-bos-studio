587feb0d17c037a94d7cb810a087031009c4216e
Dries Bos <driesbos@MBP-van-Dries.home>
2020-11-06 15:35:14 +0100

    fix: viewToggle function error

--- layouts/default.vue
+++ layouts/default.vue
@@ -99,6 +99,24 @@ export default {
         })
       }
     },
+    laserLoad() {
+      gsap.set(".laserBlok-Line", {
+        width: 0,
+        height: 0,
+        opacity: 0
+      })
+      gsap.to(".laserBlok-Line", {
+        opacity: 1,
+        duration: 0,
+        ease: "none"
+      })
+      gsap.to(".laserBlok-Line", {
+        width: "105vw",
+        height: "105vh",
+        duration: 0.66,
+        ease: "expo.out"
+      })
+    },
     viewIsToggled() {
       this.laserLoad()
       setTimeout(function() {
