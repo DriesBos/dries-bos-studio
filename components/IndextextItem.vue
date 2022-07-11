fde50e48ee8827bef0770c0b8de0ae3f0f66c2e3
DriesBos <info@driesbos.com>
2022-07-11 10:09:29 +0100

    indexblock animation

--- components/IndextextItem.vue
+++ components/IndextextItem.vue
@@ -7,34 +7,23 @@
     :class="[{ disabled: !post.active }, { cursorInteract: post.active }]"
     tag="section"
   >
-    <ul class="contentListItem">
-      <li class="contentListItem-Column year">
+    <div class="block-Text_Content">
+      <div class="block-Text_Column year">
         <p title="year">{{ post.year || "" }}</p>
-      </li>
-      <li class="contentListItem-Column title">
+      </div>
+
+      <div class="block-Text_Column title">
         <p title="project">{{ post.title || "" }}</p>
-      </li>
-      <!-- <li class="contentListItem-Column agency">
-          <p title="agency">{{ post.agency || "" }}</p>
-        </li>
-        <li class="contentListItem-Column category">
-          <p title="industry">{{ post.category || "" }}</p>
-        </li>
-        <li class="contentListItem-Column category">
-          <p title="role">role</p>
-        </li> -->
-      <li class="contentListItem-Column icons">
-        <ul class="contentListItem-Icons">
-          <li class="contentListItem-Icon contentListItem-Arrow">
-            <div
-              class="icon icon-Arrow"
-              title="view project"
-              v-html="require('~/assets/images/icon-arrow.svg?include')"
-            ></div>
-          </li>
-        </ul>
-      </li>
-    </ul>
+      </div>
+
+      <div class="block-Text_Column block-Text_Icons">
+        <div
+          class="icon icon-Arrow"
+          title="view project"
+          v-html="require('~/assets/images/icon-arrow.svg?include')"
+        ></div>
+      </div>
+    </div>
   </nuxt-link>
 </template>
 
