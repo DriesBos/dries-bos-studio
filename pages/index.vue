34785d788c6a63d07d92a43e0b13bfc88d3ad114
DriesBos <info@driesbos.com>
2022-07-13 11:01:47 +0100

    change filter wording

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@
             :class="{ active: sortByTitle }"
             @click="sortTitle"
           >
-            <p class="cursorInteract" title="sort by project">client</p>
+            <p class="cursorInteract" title="sort by project">project</p>
           </div>
         </li>
         <li class="contentListItem-Column agency">
@@ -27,7 +27,7 @@
             :class="{ active: sortByAgency }"
             @click="sortAgency"
           >
-            <p class="cursorInteract" title="sort by role">studio</p>
+            <p class="cursorInteract" title="sort by role">agency</p>
           </div>
         </li>
         <li class="contentListItem-Column category">
